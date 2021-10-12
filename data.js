80e0227019b91d62230098f96197c98899165ca3
Alexander Chabin <nibach@ya.ru>
2021-10-12 05:20:29 +0500

    Map settings moved to data.js file

---
+++ data.js
@@ -0,0 +1,38 @@
+const PLANS = [
+  {
+    title: "Старый генплан",
+    map: "plan-current.jpg",
+    legend: "legend-current.jpg",
+    old: true
+  },
+  {
+    title: "Фукнциональные зоны",
+    map: "plan-functional-zones.png",
+    legend: "legend-functional-zones.png",
+  },
+  {
+    title: "Общественный транспорт",
+    map: "plan-public-transport.jpg",
+    legend: "legend-public-transport.png"
+  },
+  {
+    title: "Транспортная инфраструктура",
+    map: "plan-transport-infrastructure.jpg",
+    legend: "legend-transport-infrastructure.jpg"
+  },
+  {
+    title: "ОМЗ в части общественного транспорта",
+    map: "plan-omz-public-transport.jpg",
+    legend: "legend-omz-public-transport.jpg",
+  },
+  {
+    title: "ОМЗ по работе с детьми",
+    map: "plan-omz-children.jpg",
+    legend: "legend-omz-children.jpg",
+  },
+  {
+    title: "ОМЗ в области образования",
+    map: "plan-omz-education.jpg",
+    legend: "legend-omz-education.jpg",
+  }
+];
