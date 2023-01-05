d8fa8d0c1cefd3a2cb7472d3772e41dcca76c2a7
Alexander Chabin <nibach@ya.ru>
2023-01-05 13:23:06 +0500

    Add types to `query` and `queryAll`

--- utils/query-dom.js
+++ utils/query-dom.js
@@ -1,7 +1,9 @@
+/** @type {function(string): Element } */
 export function query(selector) {
     return document.querySelector(selector);
 }
 
+/** @type {function(string): Array<Element> } */
 export function queryAll(selector) {
     return document.querySelectorAll(selector);
 }
