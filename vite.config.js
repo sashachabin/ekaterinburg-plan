6c79f554cf4f69a36919217a1e4ce46652e1e509
Alexander Chabin <alex.chabin@gmail.com>
2026-01-10 05:30:03 +0500

    Ceate link rel="preload" dynamically in `<head>`

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import UnpluginInjectPreload from "unplugin-inject-preload/vite";
+
+import plans from "./plans/plans.json";
+
+const defaultPlanTypeName = plans[0].name;
 
 export default defineConfig({
-    assetsInclude: ['plans/**/*.png'],
-})
+  assetsInclude: ["plans/**/*.png"],
+  plugins: [
+    // Add <link rel="preload" href="*.map-[hash].png">
+    UnpluginInjectPreload({
+      files: [
+        {
+          outputMatch: new RegExp(`${defaultPlanTypeName.replace(/ /g, ".")}\\.map-[a-zA-Z0-9_-]+\\.png$`),
+          attributes: {
+            as: "image",
+            type: "image/png",
+          },
+        },
+      ],
+    }),
+  ],
+});
